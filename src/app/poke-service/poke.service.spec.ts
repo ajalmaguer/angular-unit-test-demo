8dbe6f939048c74dd806f1b5023c87c637a82bf7
AJ Almaguer <ajalmaguer@gmail.com>
2017-08-02 23:58:49 -0700

    passed pokeservice

--- src/app/poke-service/poke.service.spec.ts
+++ src/app/poke-service/poke.service.spec.ts
@@ -2,11 +2,20 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { PokeService } from './poke.service';
 
-describe('PokeServiceService', () => {
+import { Http } from '@angular/http';
+class HttpStub {
+	get() {}
+}
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+fdescribe('PokeServiceService', () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			providers: [
 				PokeService,
+				{ provide: Http, useClass: HttpStub }
 			]
 		});
 	});
@@ -15,7 +24,12 @@ describe('PokeServiceService', () => {
 		expect(service).toBeTruthy();
 	}));
 
-	it('should get pokemon', inject([], () => {
+	it('should get pokemon', inject([PokeService, Http], (pokeService: PokeService, http: Http) => {
+		spyOn(http, 'get').and.returnValue(Observable.of(null));
+
+		pokeService.getPokemon(42);
+
+		expect(http.get).toHaveBeenCalledWith('http://pokeapi.co/api/v2/pokemon/42/');
 
 	}));
 
