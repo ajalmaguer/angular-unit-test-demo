47eb367592a12098e2fd76ea443f02e7810bbaf4
AJ Almaguer <aj.almaguer@usaepay.com>
2017-08-03 10:57:06 -0700

    finished unit testing pokeform component

--- src/app/pokeform/pokeform.component.spec.ts
+++ src/app/pokeform/pokeform.component.spec.ts
@@ -1,7 +1,24 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+// services
+import { PokeService } from '../poke-service/poke.service';
+class PokeServiceStub {
+	getPokemon() {}
+}
 
 // components
 import { PokeformComponent } from './pokeform.component';
+import { AlertComponent } from '../shared/alert/alert.component';
+@Component({
+	selector: 'app-alert',
+	template: ''
+})
+class AlertStubComponent {}
 
 describe('PokeformComponent', () => {
 	let component: PokeformComponent;
@@ -9,7 +26,14 @@ describe('PokeformComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ PokeformComponent ],
+			imports: [ ReactiveFormsModule ],
+			declarations: [ 
+				PokeformComponent,
+				AlertComponent
+			],
+			providers: [
+				{ provide: PokeService, useClass: PokeServiceStub },
+			]
 		})
 		.compileComponents();
 	}));
@@ -25,19 +49,64 @@ describe('PokeformComponent', () => {
 	});
 
 	it('can only pick a pokemon between 1 and 150', () => {
-		
+		expect(component.pokeForm.valid).toBeFalsy();
+
+		component.pokeForm.patchValue({number: 1});
+		expect(component.pokeForm.valid).toBeTruthy();
+
+		component.pokeForm.patchValue({number: 0});
+		expect(component.pokeForm.valid).toBeFalsy();
+
+		component.pokeForm.get('number').patchValue(151);
+		expect(component.pokeForm.valid).toBeFalsy();
 	});
 
 	it('should show alert and not hit api if invalid form', () => {
-		
+		expect(component.pokeForm.valid).toBeFalsy();
+
+		const pokeService = fixture.debugElement.injector.get(PokeService);
+		spyOn(pokeService, 'getPokemon');
+		spyOn(component.alert, 'newAlert');
+
+		component.getPokeInfo();
+
+		expect(component.alert.newAlert).toHaveBeenCalledWith('error', 'invalid form');
+		expect(pokeService.getPokemon).not.toHaveBeenCalled();
 	});
 
 	it('should getpokeinfo if valid form', () => {
-		
+		// setup test
+		component.pokemon = { name: 'pikachu', sprite: 'pikachu.png' };
+		component.pokeForm.patchValue({ number: 25 });
+
+		// sanity check
+		expect(component.pokeForm.valid).toBeTruthy();
+		expect(component.pokemon).not.toBeNull();
+
+		// create spies
+		spyOn(component.alert, 'newAlert');
+		const pokeService = fixture.debugElement.injector.get(PokeService);
+		spyOn(pokeService, 'getPokemon').and.returnValue(Observable.of(null));
+
+		// do the thing
+		component.getPokeInfo();
+
+		// make assertions
+		expect(component.alert.newAlert).toHaveBeenCalledWith('', 'Fetching Pokemon...');
+		expect(pokeService.getPokemon).toHaveBeenCalledWith(25);
+		expect(component.pokemon).toBeNull();
 	});
 
 	it('should display pokemon on page', () => {
+		component.pokemon = { name: 'pikachu', sprite: 'pikachu.png' };
+		fixture.detectChanges();
+
+		const pokemonDebugElement = fixture.debugElement.query(By.css('.pokemon'));
+		const imgDebugElement = fixture.debugElement.query(By.css('.pokemon img'));
 		
+		expect(pokemonDebugElement.nativeElement.textContent).toContain('pikachu');
+		expect(imgDebugElement.nativeElement.src).toContain('pikachu.png');
+
 	});
 });
 
